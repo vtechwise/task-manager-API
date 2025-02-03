b802db1cc7ccd0b9f5aec7c20b1b57cad7e58235
victor <oguntayovictor65@gmail.com>
2025-02-03 23:44:04 +0100

    refactor DBconnection, make sure the DB is connected before spinning up the express server

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
-require('./db/connect')
+const connectDB = require("./db/connect");
 const tasks = require("./routes/tasks");
 const app = express();
 
 app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 
-app.listen("5000", () => {
-  console.log("server started at port 5000");
-});
+const start = async () => {
+  try {
+    await connectDB;
+    app.listen("5000", () => {
+      console.log("server started at port 5000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://victor:fourboys@cluster0.3mpum.mongodb.net/task-manager?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("conection successfull"))
-  .catch((err) => console.log(err));
+
+const connectDB = (url) => {
+     mongoose.connect(connectionString); 
+  }
+
+module.exports = connectDB
