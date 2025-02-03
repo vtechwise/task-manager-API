bef892da38bf0a3707e189390cc77ccd2fd72b58
victor <oguntayovictor65@gmail.com>
2025-02-04 00:02:21 +0100

    save dbconnection string in .env file

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const connectDB = require("./db/connect");
 const tasks = require("./routes/tasks");
 const app = express();
+require('dotenv').config()
 
 app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 
 const start = async () => {
   try {
-    await connectDB;
+    await connectDB(process.env.mongodb_URL);
     app.listen("5000", () => {
       console.log("server started at port 5000");
     });
