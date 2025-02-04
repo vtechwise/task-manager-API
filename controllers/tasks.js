a4d554c82783c7fa6ec4279389a9545261260709
victor <oguntayovictor65@gmail.com>
2025-02-04 13:46:57 +0100

    work on getTask controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -19,8 +19,20 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  try {
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({
+        success: false,
+        msg: `No task matches the given id: ${taskID}`,
+      });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ success: false, msg: error });
+  }
 };
 const editTask = (req, res) => {
   res.send("task edited");
