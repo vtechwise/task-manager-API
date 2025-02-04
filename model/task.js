42da0b8966508415d8846a3246e8b081ebb8bde3
victor <oguntayovictor65@gmail.com>
2025-02-04 08:22:17 +0100

    add few validation to the taskSchema

--- model/task.js
+++ model/task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
 const taskSchema =new mongoose.Schema({
-    name:String,
-    completed:Boolean
+    name: {
+        type: String,
+        required: [true, 'Must include name'],
+        trim: true,
+        maxlength:[20,'Name cannot be more than 20 characters']
+    },
+    completed: {
+        type: Boolean,
+        default:false
+    }
 })
 
 module.exports = mongoose.model('task', taskSchema)
