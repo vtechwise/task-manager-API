e45515f21da3fe34538ffe5b96d423aa587fc403
victor <oguntayovictor65@gmail.com>
2025-02-04 14:12:54 +0100

    work on deleteTask controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,9 +3,9 @@ const Task = require("../model/task");
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
-    res.status(200).json({ tasks });
+    return res.status(200).json({ tasks });
   } catch (error) {
-    res, status(500).json({ success: false, msg: error });
+    return res.status(500).json({ success: false, msg: error });
   }
 };
 
@@ -31,13 +31,25 @@ const getTask = async (req, res) => {
     }
     res.status(200).json({ task });
   } catch (error) {
-    res.status(500).json({ success: false, msg: error });
+    return res.status(500).json({ success: false, msg: error });
   }
 };
 const editTask = (req, res) => {
   res.send("task edited");
 };
-const deleteTask = (req, res) => {
+
+const deleteTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  try {
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ task });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ success: false, msg: error });
+  }
+
   res.send("task deleted");
 };
 
